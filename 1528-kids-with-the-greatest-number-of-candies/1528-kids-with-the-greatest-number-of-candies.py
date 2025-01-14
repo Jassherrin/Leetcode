5b795e114ef25bae0a33024418f37e8f8820729e
Jassherrin Suther David <jassherrin@gmail.com>
2025-01-14 23:38:43 +0800

    Time: 0 ms (100%), Space: 12.4 MB (43.98%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        maxCandies = max(candies)
+        
+        return [(curNum + extraCandies >= maxCandies) for curNum in candies]
